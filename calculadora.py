b2afdf9018d59d5f9371497fdf1541b6062fd889
manolito <ayalamirkoemanuel@gmail.com>
2024-08-28 18:16:45 -0400

    codigoerror

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,22 @@
-nombre= int(input("ingrese un numero "))
-print(nombre)
+int =print("ingrse un numero ")
+def suma(a,b):
+    return  a+b 
 
-print(type(nombre))
-#m
+def resta(a,b):
+    return a-b
+
+def multiplicación(a,b):
+    return a*b
+
+def division(a,b):
+    if b==0:
+        return "error de division"
+    return a/b
+
+def calculator():
+    print("Seleccione operación:")
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Multiplicar")
+    print("4. Dividir")
+    
---
+++ codiguiño.py
@@ -0,0 +1,5 @@
+nombre= input("ingrese nombre " )
+
+a=int(input("ingrese el numero "))
+
+print(type(a))
